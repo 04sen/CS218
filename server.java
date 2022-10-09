1bb31b26399cbab97f52eb74be9fbe9cc421199e
04.Sen <84554409+SachinandanSen@users.noreply.github.com>
2022-10-09 16:03:41 +1200

    GameFrame.java (v0.3.1) and Additional QoL changes

--- server.java
+++ server.java
@@ -1,9 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
-package rpsls;
-
 import java.io.*;
 import java.net.*;
 
@@ -44,61 +38,6 @@ public class server {
             server server = new server (serverSocket);
             server.startServer();
         }
-    
-//    public static void main(String args[]) throws IOException {
-//        Socket socket = null;
-//        InputStreamReader inputStreamReader = null;
-//        OutputStreamWriter outputStreamWriter = null;
-//        BufferedReader bufferedReader = null;
-//        BufferedWriter bufferedWriter = null;
-//        ServerSocket serverSocket = null;
-//        
-//        serverSocket = new ServerSocket (1234);
-//        
-//        while (true) {
-//            try{
-//                socket = serverSocket.accept();
-//                
-//                
-//                inputStreamReader = new InputStreamReader(socket.getInputStream());
-//                outputStreamWriter = new OutputStreamWriter (socket.getOutputStream());
-//                
-//                bufferedReader = new BufferedReader(inputStreamReader);
-//                bufferedWriter = new BufferedWriter(outputStreamWriter);
-//                
-//                while (true) {
-//                     String PlayerOneMove = bufferedReader.readLine();
-//                     String PlayerTwoMove = bufferedReader.readLine();
-//
-//                    for (int i = 0; i < 2; i++) 
-//                    {
-//                       
-//                        System.out.println("Client: " + PlayerOneMove);
-//
-//                        System.out.println("Client: " + PlayerTwoMove);
-//
-//                        bufferedWriter.write ("Move saved");
-//                        bufferedWriter.newLine();
-//                        bufferedWriter.flush();  
-//                    }
-//                    
-//                    
-//             //       CheckWin(PlayerOneMove, PlayerTwoMove);
-//                    
-//                    if (PlayerTwoMove.equalsIgnoreCase("Quit"))
-//                        break;
-//                }
-//                socket.close();
-//                inputStreamReader.close();
-//                outputStreamWriter.close();
-//                bufferedReader.close();
-//                bufferedWriter.close();
-//                
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
 
 //    private static void CheckWin(String PlayerOneMove, String PlayerTwoMove) {
 //        
@@ -172,7 +111,7 @@ public class server {
 //          System.out.println("Lizard poisons Spock. You win!");
 //        }
 //      }
-//      
+//
 //      else if (PlayerOneMove.equals("k")) {
 //        if (PlayerTwoMove.equals("p")) {
 //          System.out.println("Paper disproves Spock. You lose!");
