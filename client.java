1674dfe7daa302010a643167bcf9c866666a41b1
04.Sen <84554409+SachinandanSen@users.noreply.github.com>
2022-10-09 23:13:41 +1200

    QoL Changes

--- client.java
+++ client.java
@@ -1,17 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package rpsls;
-
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
-/**
- *
- * @author PC
- */
 public class client {
         
         //declare and initialize socket 
