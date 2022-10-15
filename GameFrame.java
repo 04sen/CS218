fb3fa14236e8b7e280d49859ed8ae20ac34be641
04.Sen <84554409+SachinandanSen@users.noreply.github.com>
2022-10-15 15:52:13 +1200

    QoL Changes

--- GameFrame.java
+++ GameFrame.java
@@ -1,9 +1,11 @@
-import javax.sound.sampled.*;
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.sound.sampled.*;
+import javax.swing.*;
 import java.io.*;
 import java.net.*;
+import javax.imageio.ImageIO;
+
 public class GameFrame extends JFrame{
     JButton rock_btn = new JButton();
     JButton paper_btn = new JButton();
@@ -15,20 +17,18 @@ public class GameFrame extends JFrame{
 
     ImageIcon sound_img = new ImageIcon("Imgs\\sound.png");
     ImageIcon mute_img = new ImageIcon("Imgs\\mute.png");
-
     ImageIcon rock_img = new ImageIcon("Imgs\\Rock.png");
-    JLabel rock_lb = new JLabel();
     ImageIcon paper_img = new ImageIcon("Imgs\\paper.png");
-    JLabel paper_lb = new JLabel();
     ImageIcon scissors_img = new ImageIcon("Imgs\\scissors.png");
-    JLabel scissors_lb = new JLabel();
     ImageIcon lizard_img = new ImageIcon("Imgs\\lizard.png");
-    JLabel lizard_lb = new JLabel();
     ImageIcon spock_img = new ImageIcon("Imgs\\spock.png");
-    JLabel spock_lb = new JLabel();
-    ImageIcon background = new ImageIcon("Imgs\\background.jfif");
+    ImageIcon background = new ImageIcon("Imgs\\background.jpg");
 
-   
+    JLabel rock_lb = new JLabel();
+    JLabel paper_lb = new JLabel();
+    JLabel scissors_lb = new JLabel();
+    JLabel lizard_lb = new JLabel();
+    JLabel spock_lb = new JLabel();
 
     JLabel title_lb = new JLabel();
     JLabel rules_lb = new JLabel();
@@ -56,6 +56,11 @@ public class GameFrame extends JFrame{
     }
 
     public GameFrame(){
+        try{
+            this.setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("Imgs\\background.jpg")))));
+        } catch (IOException e) {
+            System.out.println("Unable to load background.");
+        }
     }
 
     public void setUpGUI() throws UnsupportedAudioFileException, IOException, LineUnavailableException{
@@ -64,7 +69,8 @@ public class GameFrame extends JFrame{
          audioStream = AudioSystem.getAudioInputStream(audio_file);
          clip = AudioSystem.getClip();
          clip.open(audioStream);
- 
+
+
          //Setting Bounds for Labels
          rock_lb.setText("You have chosen Rock!");
          rock_lb.setIcon(rock_img);
@@ -72,6 +78,7 @@ public class GameFrame extends JFrame{
          rock_lb.setBounds(350, 500, 500,130);
          rock_lb.setVisible(false);
          rock_lb.setForeground(Color.decode("#ffb7c5"));
+         rock_lb.setFont(new Font("Nyala", Font.PLAIN , 16));
  
          paper_lb.setText("You have chosen Paper!");
          paper_lb.setIcon(paper_img);
@@ -79,6 +86,7 @@ public class GameFrame extends JFrame{
          paper_lb.setBounds(350, 500, 500,130);
          paper_lb.setVisible(false);
          paper_lb.setForeground(Color.decode("#ffb7c5"));
+         paper_lb.setFont(new Font("Nyala", Font.PLAIN  , 16));
  
          scissors_lb.setText("You have chosen Scissors!");
          scissors_lb.setIcon(scissors_img);
@@ -86,6 +94,7 @@ public class GameFrame extends JFrame{
          scissors_lb.setBounds(350, 500, 500,130);
          scissors_lb.setVisible(false);
          scissors_lb.setForeground(Color.decode("#ffb7c5"));
+         scissors_lb.setFont(new Font("Nyala", Font.PLAIN  , 16));
  
          lizard_lb.setText("You have chosen Lizard!");
          lizard_lb.setIcon(lizard_img);
@@ -93,6 +102,7 @@ public class GameFrame extends JFrame{
          lizard_lb.setBounds(350, 500, 500,130);
          lizard_lb.setVisible(false);
          lizard_lb.setForeground(Color.decode("#ffb7c5"));
+         lizard_lb.setFont(new Font("Nyala", Font.PLAIN  , 16));
  
          spock_lb.setText("You have chosen Spock!");
          spock_lb.setIcon(spock_img);
@@ -100,73 +110,92 @@ public class GameFrame extends JFrame{
          spock_lb.setBounds(350, 500, 500,150);
          spock_lb.setVisible(false);
          spock_lb.setForeground(Color.decode("#ffb7c5"));
+         spock_lb.setFont(new Font("Nyala", Font.PLAIN  , 16));
  
-         title_lb.setText("<html>Welcome to Rock Paper Scissors Lizard Spock!<br>You are Player #" + playerID + "<html>");
-         title_lb.setBounds(350, 0, 500,50);
+         title_lb.setText("<html>Welcome to Rock Paper Scissors Lizard Spock!<br> <br> You are Player #" + playerID + "<html>");
+         title_lb.setBounds(330, 0, 500,100);
          title_lb.setVisible(true);
          title_lb.setForeground(Color.decode("#ffb7c5"));
+         title_lb.setFont(new Font("Nyala", Font.PLAIN  , 16));
  
          rules_lb.setText("<html>Rules:<br>Scissors cuts Paper<br>Paper covers Rock<br>Rock crushes Lizard<br>Lizard poisons Spock<br>Spock smashes Scissors<br>Scissors decapitates Lizard<br>Lizard eats Paper<br>Paper disproves Spock<br>Spock vaporizes Rock<br>Rock crushes Scissors<html>");
-         rules_lb.setBounds(10, 0, 200,400);
+         rules_lb.setBounds(20, 10, 200,400);
          rules_lb.setVisible(true);
          rules_lb.setForeground(Color.decode("#ffb7c5"));
+         rules_lb.setFont(new Font("Nyala", Font.PLAIN , 16));
  
          waiting_lb.setText(".....Waiting for Other Players......");
          waiting_lb.setBounds(380, 200, 500,50);
          waiting_lb.setVisible(false);
          waiting_lb.setForeground(Color.decode("#ffb7c5"));
+         waiting_lb.setFont(new Font("Nyala", Font.PLAIN  , 16));
  
          lost_lb.setText(".....You Have Lost!......");
          lost_lb.setBounds(380, 200, 500,50);
          lost_lb.setVisible(false);
          lost_lb.setForeground(Color.decode("#ffb7c5"));
+         lost_lb.setFont(new Font("Nyala", Font.PLAIN , 16));
  
          win_lb.setText(".....You Have Won!......");
          win_lb.setBounds(380, 200, 500,50);
          win_lb.setVisible(false);
          win_lb.setForeground(Color.decode("#ffb7c5"));
+         win_lb.setFont(new Font("Nyala", Font.PLAIN , 16));
  
          tie_lb.setText(".....Its a Tie! Try Again!......");
          tie_lb.setBounds(380, 200, 500,50);
          tie_lb.setVisible(false);
          tie_lb.setForeground(Color.decode("#ffb7c5"));
+         tie_lb.setFont(new Font("Nyala", Font.PLAIN , 16));
  
  
          //Setting Bounds for Buttons
-         rock_btn.setBounds(200, 400, 90, 50); 
-         rock_btn.setText("Rock"); 
+         rock_btn.setBounds(230, 400, 90, 50); 
+         rock_btn.setText("Rock");
+         rock_btn.setFont(new Font("Arial", Font.BOLD, 18));
+         rock_btn.setBackground(Color.decode("#dca9fc"));
          rock_btn.setFocusable(false); 
          
-         paper_btn.setBounds(310, 400, 90, 50); 
+         paper_btn.setBounds(340, 400, 90, 50); 
          paper_btn.setText("Paper"); 
+         paper_btn.setFont(new Font("Arial", Font.BOLD, 18));
+         paper_btn.setBackground(Color.decode("#dca9fc"));
          paper_btn.setFocusable(false); 
          
-         scissors_btn.setBounds(420, 400, 90, 50); 
-         scissors_btn.setText("Scissors"); 
+         scissors_btn.setBounds(450, 400, 90, 50); 
+         scissors_btn.setText("Scissors");
+         scissors_btn.setFont(new Font("Arial", Font.BOLD , 18));
+         scissors_btn.setBackground(Color.decode("#dca9fc"));
          scissors_btn.setFocusable(false); 
         
-         lizard_btn.setBounds(530, 400, 90, 50); 
+         lizard_btn.setBounds(560, 400, 90, 50); 
          lizard_btn.setText("Lizard"); 
+         lizard_btn.setFont(new Font("Arial", Font.BOLD, 18));
+         lizard_btn.setBackground(Color.decode("#dca9fc"));
          lizard_btn.setFocusable(false); 
          
-         spock_btn.setBounds(640, 400, 90, 50); 
+         spock_btn.setBounds(670, 400, 90, 50); 
          spock_btn.setText("Spock"); 
+         spock_btn.setFont(new Font("Arial", Font.BOLD, 18));
+         spock_btn.setBackground(Color.decode("#dca9fc"));
          spock_btn.setFocusable(false); 
-
+         
          stopMusic_btn.setBounds(850,10,50,50);
          stopMusic_btn.setIcon(sound_img);
          stopMusic_btn.setFocusable(false);
-
+         stopMusic_btn.setBackground(Color.decode("#dca9fc"));
+         
          playMusic_btn.setBounds(850,10,50,50);
          playMusic_btn.setIcon(mute_img);
          playMusic_btn.setVisible(false);
          playMusic_btn.setFocusable(false);
- 
+         playMusic_btn.setBackground(Color.decode("#dca9fc"));
+         
          //Setting Bounds for the JFrame
-         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         setResizable(false);
+         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         this.setResizable(false);
          this.setLayout(null);
-         setSize(1000,700);
+         this.setSize(1000,700);
          
          //Allows the JFrame to appear in the  middle of the screeen
          setLocationRelativeTo(null);
@@ -195,7 +224,7 @@ public class GameFrame extends JFrame{
          this.add(win_lb);
          this.add(lost_lb);
          this.add(tie_lb);
- 
+          
          //Starting Background Audio once JFrame is opened
          clip.start();
     }
@@ -288,14 +317,18 @@ public class GameFrame extends JFrame{
                 playerID = dis.readInt();
                 
             
-            } catch (IOException e) {}
+            } catch (IOException e) {
+                System.out.println("Error in ClientSideConnection constructor");
+            }
         }
 
         public void sendChoice(int n){
             try{
                 dos.writeInt(n);
                 dos.flush();
-            }catch(IOException e){}
+            }catch(IOException e){
+                System.out.println("Error in sendChoice()");
+            }
         }
 
         public void getResult(){
